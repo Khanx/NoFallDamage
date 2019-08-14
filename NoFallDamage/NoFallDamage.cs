e77fa2ecc5f409589717ccf6295bfb102c58fd0a
Khanx <maximuskhanx@hotmail.com>
2019-08-14 22:54:57 +0200

    Update Callback identifier

--- NoFallDamage/NoFallDamage.cs
+++ NoFallDamage/NoFallDamage.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NoFallDamage
+﻿namespace NoFallDamage
 {
     [ModLoader.ModManager]
     public static class NoFallDamage
     {
-        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerHit, "Khanx.SimpleFluids.NotKillPlayerOnHitWater")]
-        public static void NoDamage(Players.Player player, ModLoader.OnHitData d)
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerHit, "Khanx.NoFallDamage.OnPlayerHit")]
+        public static void OnPlayerHit(Players.Player player, ModLoader.OnHitData d)
         {
             if (null == player || null == d || d.HitSourceType != ModLoader.OnHitData.EHitSourceType.FallDamage)
                 return;
