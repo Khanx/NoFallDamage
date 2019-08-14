5d03a32f7bdaafe65d04d08eac43e0f8054e3c64
Khanx <maximuskhanx@hotmail.com>
2019-08-14 21:11:30 +0200

    No Fall Damage

---
+++ NoFallDamage/NoFallDamage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoFallDamage
+{
+    [ModLoader.ModManager]
+    public static class NoFallDamage
+    {
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerHit, "Khanx.SimpleFluids.NotKillPlayerOnHitWater")]
+        public static void NoDamage(Players.Player player, ModLoader.OnHitData d)
+        {
+            if (null == player || null == d || d.HitSourceType != ModLoader.OnHitData.EHitSourceType.FallDamage)
+                return;
+
+            d.ResultDamage = 0;
+        }
+
+    }
+}
